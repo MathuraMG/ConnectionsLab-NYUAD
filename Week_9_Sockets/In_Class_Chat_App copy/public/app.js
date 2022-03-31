42ea07ca06f42308db9016eb6048556499bb7112
Mathura MG <mmg9291@gmail.com>
2022-03-31 10:26:37 +0400

    add class 2

---
+++ Week_9_Sockets/In_Class_Chat_App copy/public/app.js
@@ -0,0 +1,58 @@
+// client connects to the server
+let socket = io();
+
+//confirm that the client is connected
+socket.on('connect',() => {
+  console.log('connected to the server');
+  let data = {
+    'name': sessionStorage.getItem('name'),
+    'room': sessionStorage.getItem('room')
+  }
+  socket.emit('roomJoin', data);
+})
+
+//receive old messages
+socket.on('pastMessages', (data) => {
+  console.log(data);
+})
+
+socket.on('userTyping', () => {
+  console.log('user is typing');
+})
+
+//receive the information from the server
+socket.on('chatMessage',(data) => {
+  console.log(data);
+  let chatWindow = document.getElementById('chat-box-msgs');
+  let chatMessageArea = document.createElement('p');
+  chatMessageArea.innerHTML = data.name +  " : " + data.msg;
+  chatWindow.appendChild(chatMessageArea);
+})
+
+//2) get the input from the user, on click event we get the information (c)
+
+window.addEventListener('load', () => {
+
+  let submitButton = document.getElementById('send-button');
+  
+  submitButton.addEventListener('click', () => {
+    let name = sessionStorage.getItem('name');
+    let msg = document.getElementById('msg-input').value;
+    console.log(name, msg);
+
+    //emit the information to the server
+    chatObject = {
+      'name': name,
+      'msg': msg
+    }
+    socket.emit('chatMessage', chatObject);
+  })
+
+  //code to see if any user is typing
+  let messageInput = document.getElementById('msg-input');
+  messageInput.onkeypress = () => {
+    console.log('typing')
+    socket.emit('userTyping');
+  }
+
+})
--- Week_9_Sockets/README.md
+++ Week_9_Sockets/README.md
@@ -10,3 +10,23 @@
 * Steps we will do together
   * set up the flow of information from client to server and back
   * store that information in the server - share it with new joiners
+
+# Class 2
+
+* Discuss ideas -> https://docs.google.com/document/d/1RdKYnMEL2qKGOnTtIyaWxzfu1h-_u8zPM6VdCCOFC3c/edit
+* Homework review
+
+## Namespaces and rooms
+* A Namespace is a communication channel that allows you to split the logic of your application over a single shared connection (also called "multiplexing").
+* A room is an arbitrary channel that sockets can join and leave. It can be used to broadcast events to a subset of clients
+* Namespaces: are managed in the frontend meaning the user, or an attacker, joins through the frontend and the joining and disconnecting is managed here. C -> `let sockets = io.of('/namespace1)`
+* Rooms: are managed in the backend, meaning the server assigns joining and leaving rooms. `socket.join(roomName)`
+
+* DEMO Trivia Example - https://glitch.com/edit/#!/ima-cl-21-trivia-app 
+  * Input Page -https://ima-cl-21-trivia-app.glitch.me/input
+  * Output Page - https://ima-cl-21-trivia-app.glitch.me/output
+* DEMO Private Chat Rooms - Github = https://github.com/MathuraMG/IMA-Low-Res-Connections-Lab/tree/master/Week_09%7CMore_with_sockets/Chat_App_With_Rooms 
+
+## Killing stale servers
+* ps -ef  grep index.js
+* kill -9 PID
