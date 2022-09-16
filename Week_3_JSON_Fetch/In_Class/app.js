607896536490dbf9fcdf13b6d04f9a74a60da448
Mathura MG <mmg9291@gmail.com>
2022-09-16 16:43:11 +0530

    add week 3 in class work

--- Week_3_JSON_Fetch/In_Class/app.js
+++ Week_3_JSON_Fetch/In_Class/app.js
@@ -1,51 +1,35 @@
-let allColours;
-window.addEventListener("load", () => {
-  console.log("page is loaded");
-
-  //*****************/
-  //Display the colour inside the colour box
-  //*****************/
-
-
-
-  fetch("./crayola.json") // fetching the information from the URL/ file
-  .then( response  =>  response.json() )  // returning the Promise Object which contains data
-  .then((data) => {
-    console.log(data); // seeing the data
-    allColours = data.colors;
-    // Create html elements that are of the colours in the array
-
-    for(let i=0; i<allColours.length;i++) {
-      // debugger;
-
-      // create an li for each colour , and give it the class "list__item"
-      let listItem = document.createElement('li');
-      listItem.classList.add("list__item");
-      // add style backgroundColor to each item
-      listItem.style.backgroundColor = allColours[i].hex;
-      listItem.textContent = allColours[i].hex;
-
-
-      //add a p tag
-      let listItemColor = document.createElement("p");
-      listItemColor.classList.add("list__item-color");
-      listItemColor.innerHTML = allColours[i].color;
-
-      // get the list element using the id "list"
-      let list = document.getElementById("list");
-      
-      //append the li to the ul
-      list.appendChild(listItem);
-      listItem.appendChild(listItemColor);
-
-    }
-
+window.addEventListener('load', function() {
+  fetch('https://api.le-systeme-solaire.net/rest/bodies') //access the json
+  .then(response => response.json())
+  .then(data => { // access the data IF the promise is fulfilled
+    console.log(data);
+    let desc = data.bodies[0].name; // accessing the "description" property of the object "data"
+    let p = document.createElement('p');
+    p.innerHTML = desc;
+    p.classList.add("content");
+    document.body.appendChild(p);
   })
-
+  .catch(e => {
+    console.log('there is an error', e);
+  })
 })
 
-// 1. remove "function()arg" -- "(arg) => "
-// 2. remove the {} if there is only online, and delete return
-// 3. if its only 1 argument, you can remove the brackets. ie " (arg) =>"  -- "arg =>"
+
+
+
+// To go over the button event lsiteners//
+/*
+window.addEventListener('load', function() {
+  //get access to all the buttons
+  let buttons = document.getElementsByClassName('button');
+  //iterating through the buttons
+  for(let i =0;i<buttons.length;i++) {
+    console.log(buttons[i]);
+    buttons[i].addEventListener('click',function(e) {
+      console.log(e.target.dataset.fruit);
+    })
+  }
+})
+*/
 
 
